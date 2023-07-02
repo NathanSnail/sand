006e580bdd73b3cdbee19aa017948da18ed0eda0
Dice Toss <dicetoss@pengiun>
2023-07-03 01:23:41 +1200

    sand

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ world = np.zeros(size,dtype="uint8")
 # 0: air
 # 1-2: water
 # 3-4: sand
-
+# 5-6: rock
 # ===== Render ======
 
-col_map = [(curses.COLOR_BLACK,curses.COLOR_BLACK),(curses.COLOR_BLUE,curses.COLOR_BLUE)]
+col_map = [
+        (curses.COLOR_BLACK,curses.COLOR_BLACK),
+        (curses.COLOR_BLUE,curses.COLOR_BLUE),
+        (curses.COLOR_YELLOW,curses.COLOR_YELLOW)
+]
 
 def render(scr):
     for y in range(size[1]):
@@ -38,31 +42,52 @@ def tick():
     global world
     for y in range(size[1]):
         for x in range(size[0]):
-            func_map[(world[x,y]-1)//2](x,y)
+            material = world[x,y]
+            if material % 2 == 0:
+                continue
+            func_map[world[x,y]//2](x,y)
 
 def tick_water(x,y): 
     # unticked pixel
-    if world[x,y] == 1:
-        if y == 0:
-            # bottom of world
+    if y == 0:
+        # bottom of world
+        return
+    if world[x,y-1] == 0:
+        # empty space
+        world[x,y-1] = 2
+        world[x,y] = 0
+    else:
+        # filled space
+        direction = random.choice([-1,1])
+        nx = x + direction
+        if nx < 0 or nx >= size[0]:
             return
-        if world[x,y-1] == 0:
-            # empty space
-            world[x,y-1] = 2
+        if world[nx,y] == 0:
+            # empty space next to
+            world[nx,y] = 2
             world[x,y] = 0
-        else:
-            # filled space
-            direction = random.choice([-1,1])
-            nx = x + direction
-            if nx >= 0 and nx < size[0]:
-                if world[nx,y] == 0:
-                    # empty space next to
-                    world[nx,y] = 2
-                    world[x,y] = 0
 
 def tick_sand(x,y):
-    pass
-
+    if y == 0:
+        # bottom of world
+        return
+    below = world[x,y-1]
+    if below < 3 and random.random() < 0.8: # chance to move with water
+        pass
+        # empty space
+        world[x,y-1] = 4
+        world[x,y] = below
+    else:
+        # filled space
+        direction = random.choice([-1,1])
+        nx = x + direction
+        if nx < 0 or nx >= size[0]:
+            return
+        below_side = world[nx,y-1]
+        if below_side < 3:
+            # empty space next to
+            world[nx,y] = 4
+            world[x,y] = below_side
 
 func_map = [tick_water,tick_sand]
 
@@ -77,7 +102,12 @@ def clean():
 def world_init():
     for y in range(size[1]):
         for x in range(size[0]):
-            world[x,y] = 1 if random.random() < 0.1 else 0
+            ty = 0
+            if random.random() < 0.1:
+                ty = 1
+            elif random.random() < 0.1:
+                ty = 3
+            world[x,y] = ty
 
 # ===== Control =====
 
@@ -86,11 +116,15 @@ def init():
     world_init()
 
 def main(scr):
+    if not curses.can_change_color():
+        raise Exception("Doesn't support fancy colours")
     scr.nodelay(True)
     init()
     while True:
         #scr.clear()
         scr.erase() # magically fixes all the flickering
+        #time.sleep(0.5)
+        scr.addstr(str())
         tick()
         clean()
         render(scr)
