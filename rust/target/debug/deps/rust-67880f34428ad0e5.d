e1cf4002f98d3cceb233521f7bb5f5394926e9b3
Dice Toss <dicetoss@pengiun>
2023-07-09 23:00:51 +1200

    wok

---
+++ rust/target/debug/deps/rust-67880f34428ad0e5.d
@@ -0,0 +1,5 @@
+/home/dicetoss/sand/rust/target/debug/deps/rust-67880f34428ad0e5: src/main.rs
+
+/home/dicetoss/sand/rust/target/debug/deps/rust-67880f34428ad0e5.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/dicetoss/sand/rust/target/debug/rust: /home/dicetoss/sand/rust/src/main.rs
